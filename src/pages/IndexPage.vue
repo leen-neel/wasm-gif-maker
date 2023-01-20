d3c856ba5059f9ad626c4c0bd4fec2273a72664c
leen-neel <and24903@gmail.com>
2023-01-20 10:53:03 +0530

    now it runs

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,26 +1,38 @@
 <template>
   <q-page class="q-pa-md">
-    <q-file
-      v-model="file"
-      label="Pick one file"
-      filled
-      style="max-width: 300px"
-      accept="video/*"
-      class="q-mb-sm"
-    />
-    <q-btn color="primary" icon="upload" label="Upload" @click="getFiles" />
-    {{ ready }}
+    <div v-if="!ready">Loading....</div>
+
+    <div v-if="ready">
+      <!-- <input type="file" @change="(e) => getFiles(e.target.files?.item(0))" /> -->
+
+      <q-file
+        v-model="file"
+        label="Pick one file"
+        filled
+        style="max-width: 300px"
+        class="q-mb-sm"
+      />
+      <q-btn
+        color="primary"
+        icon="upload"
+        label="Upload"
+        @click="getFiles(file)"
+      />
+
+      <q-video :src="url" style="max-width: 300px" />
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
 import { ref } from 'vue';
-import { createFFmpeg } from '@ffmpeg/ffmpeg';
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 
 export default {
   setup() {
-    const file = ref<FileList>();
+    const file = ref<File>();
     const ready = ref(false);
+    const url = ref('');
 
     const ffmpeg = createFFmpeg({
       log: true,
@@ -33,14 +45,33 @@ export default {
 
     load();
 
-    const getFiles = () => {
-      console.log(file.value);
+    const gifConv = async (vid: File) => {
+      ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(vid));
+
+      await ffmpeg.run(
+        '-i',
+        'test.mp4',
+        '-t',
+        '2.5',
+        '-ss',
+        '2.0',
+        '-f',
+        'gif',
+        'out.gif'
+      );
+    };
+
+    const getFiles = (fileName: File) => {
+      console.log(fileName);
+      url.value = URL.createObjectURL(fileName);
+      gifConv(fileName);
     };
 
     return {
       file,
       ready,
       getFiles,
+      url,
     };
   },
 };
